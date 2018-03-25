64075399f8ef0cb66af2fe783019e9c7a812f12d
Nikita Ermolenko <gnod94@gmail.com>
2018-03-25 13:49:21 +0600

    Generate xcode project

---
+++ iOS-smooth-scrolling/iOS-smooth-scrolling/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS-smooth-scrolling
+//
+//  Created by Nikita Ermolenko on 25/03/2018.
+//  Copyright © 2018 Nikita Ermolenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
